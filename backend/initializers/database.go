b4b4dcae629a7de150789cd169b805d3945ce696
alisalim17 <alisalimli1899@gmail.com>
2023-03-21 13:32:49 +0400

    connecting database configuration

--- backend/initializers/database.go
+++ backend/initializers/database.go
@@ -6,6 +6,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -14,6 +15,16 @@ func ConnectToDatabase() {
 	var err error
 	dsn := os.Getenv("DB_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the Database! \n", err.Error())
+		os.Exit(1)
+	}
+
+	DB.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+	DB.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("🚀 Connected Successfully to the Database")
 
 	if err != nil {
 		log.Fatal("Failed to connect a database.")
--- backend/migrations/migrate.go
+++ backend/migrations/migrate.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/alisalimli/bookstore/backend/initializers"
 	"github.com/alisalimli/bookstore/backend/models"
 )
@@ -11,6 +14,11 @@ func init() {
 }
 
 func main() {
-	initializers.DB.AutoMigrate(&models.Post{})
+	log.Println("Running Migrations")
+	err := initializers.DB.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatal("Migration Failed:  \n", err.Error())
+		os.Exit(1)
+	}
 
 }
